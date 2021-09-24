fb51f88e7e092a09d4f4a4bb9a855a9e74650a50
Aristevao <aristevao@hotmail.com>
2021-09-24 13:57:40 -0300

    [T-02] Translating some error messages

--- api/billingCycleService.js
+++ api/billingCycleService.js
@@ -4,4 +4,16 @@ const BillingCycle = require('./billingCycle');
     Create a API rest as pattern to the BillingCycle object */
 BillingCycle.methods(['get', 'post', 'put', 'delete']);
 
+/*  This config <new: true> assure that when updated a
+    document the response will contain the 
+    new object and don't the previous, as
+    default
+    
+    runValidators -> will emit an error if the sent 
+    document don't match with the schema. 
+    Ex: required:true. Will emit an error if this 
+    attribute is not sent.
+    As default runValidator = false */
+BillingCycle.updateOptions({ new: true, runValidators: true });
+
 module.exports = BillingCycle;
