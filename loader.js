2fd57ea866e0e0be8a0482ac61184bee9304cb76
Aristevao <aristevao@hotmail.com>
2021-09-24 08:14:40 -0300

    [T-02] Start server

---
+++ loader.js
@@ -0,0 +1,11 @@
+const port = 3003;
+
+const express = require('express');
+const server = express();
+
+server.use(express.json()); // json parse
+server.use(express.urlencoded({ // parse of data from form submission
+  extended: true
+}));
+
+server.listen(port, () => console.log(`Server is listening on http://localhost:${port}`));
