231dbd0f63ed8f614dc2ca2c8a95b76f562f8c0b
Aristevao <aristevao@hotmail.com>
2021-09-24 11:40:12 -0300

    [T-02] register billingCycles

--- config/routes.js
+++ config/routes.js
@@ -4,5 +4,11 @@ module.exports = server => {
     
     // API Routes
     const router = express.Router();
-    server.use('/api', router) ;// "use" defines '/api' as default route 
+    /*  "use" defines '/api' as default route.
+        Every endpoint needs to contain '/api'
+        Ex: /api/test  */
+    server.use('/api', router);
+
+    const billingCycleService = require('../api/billingCycleService');
+    billingCycleService.register(router, '/billingCycles');
 };
