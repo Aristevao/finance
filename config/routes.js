38a7ac742b929a08308467d94ec19fb14cf0325f
Aristevao <aristevao@hotmail.com>
2021-09-27 09:02:53 -0300

    [T-03] Does not working any methods

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 module.exports = server => {
-    
+
     // API Routes
     const router = express.Router();
     /*  "use" defines '/api' as default route.
@@ -9,6 +9,9 @@ module.exports = server => {
         Ex: /api/test  */
     server.use('/api', router);
 
-    const billingCycleService = require('../api/billingCycleService');
+    const billingCycleService = require('../api/billingCycle/billingCycleService');
     billingCycleService.register(router, '/billingCycles');
+
+    const billingSummaryService = require('../api/billingSummary/billingSummaryService');
+    router.route('/billingSummary').get(billingSummaryService.getSummary); 
 };
