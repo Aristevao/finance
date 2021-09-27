ca02a93f0c9b56bdb3181e96a974d390600a2bb3
Aristevao <aristevao@hotmail.com>
2021-09-27 10:06:01 -0300

    [T-03] Treating errors messages

--- api/billingCycle/billingCycleService.js
+++ api/billingCycle/billingCycleService.js
@@ -1,4 +1,6 @@
+const res = require('express/lib/response');
 const BillingCycle = require('./billingCycle');
+const _ = require('lodash');
 
 /*  "methods" from node-restful
     Create a API rest as pattern to the BillingCycle object */
@@ -15,6 +17,26 @@ BillingCycle.methods(['get', 'post', 'put', 'delete']);
     attribute is not sent.
     As default runValidator = false */
 BillingCycle.updateOptions({ new: true, runValidators: true });
+
+/* Treats errors messages */
+BillingCycle.after('post', sendErrorsOrNext).after('put', sendErrorsOrNext);
+
+function sendErrorsOrNext() {
+    const bundle = res.locals.errors;
+
+    if (bundle.errors) {
+        const errors = parseErrors(bundle.errors);
+        res.status(500).json({ errors });
+    } else {
+        next();
+    }
+}
+ 
+function parseErrors(nodeRestfulErrors) {
+    const errors = [];
+    _.forIn(nodeRestfulErrors, error => errors.push(error.message));
+    return errors;
+}
 
 // Serviço Contador (count) de Ciclo de Pagamentos
 BillingCycle.route('count', (req, res, next) => {
